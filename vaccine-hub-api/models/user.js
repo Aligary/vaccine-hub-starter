88824b495a6577e37df32a6b50b2e4330b71b0a8
Aligary <aligaryjr@gmail.com>
2022-06-28 14:43:51 -0700

    Finished

--- vaccine-hub-api/models/user.js
+++ vaccine-hub-api/models/user.js
@@ -4,14 +4,37 @@ const { BCRYPT_WORK_FACTOR } = require("../config")
 const { BadRequestError, UnauthorizedError } = require("../utils/errors")
 
 class User {
+    static async makePublicUser(user) {
+        return {
+            id: user.id,
+            email: user.email,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            loation: user.location,
+            date: user.date
+        }
+    }
+
     static async login(credentials) {
         //user should submit email and password
         //if any field missing throw an error
+        const requiredFields = ["password", "email"]
+        requiredFields.forEach((e) => {
+            if(!credentials.hasOwnProperty(e)) {
+                throw new BadRequestError(`Missing ${e} in request body.`)
+            }
+        })
 
         //lookup user in db by email
+        const user = await User.fetchByUserEmail(credentials.email)
         //if user found, compare submitted pw with pw in db
         //if match, return user
-
+        if (user) {
+            const isValid = await bcrypt.compare(credentials.password, user.password)
+            if (isValid) {
+                return User.makePublicUser(user)
+            }
+        }
         //if any goes wrong throw an error
         throw new UnauthorizedError("Invalid email/password")
     }
@@ -57,7 +80,7 @@ class User {
         //return user
         const user = result.rows[0]
 
-        return user
+        return User.makePublicUser(user)
     }
 
     static async fetchByUserEmail(email) {
