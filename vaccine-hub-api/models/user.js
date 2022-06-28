7787125134882494feb1f1f331b99f5d85984460
Aligary <aligaryjr@gmail.com>
2022-06-28 14:16:41 -0700

    working registration system

--- vaccine-hub-api/models/user.js
+++ vaccine-hub-api/models/user.js
@@ -1,5 +1,5 @@
 const db = require("../db")
-const { UnauthorizedError } = require("../utils/errors")
+const { BadRequestError, UnauthorizedError } = require("../utils/errors")
 
 class User {
     static async login(credentials) {
@@ -17,16 +17,58 @@ class User {
     static async register(credentials) {
         //user should submit email, pw
         //if any missing, throw an error
+        const requiredFields = ["password", "email", "first_name", "last_name", "location"]
+        requiredFields.forEach((e) => {
+            if(!credentials.hasOwnProperty(e)) {
+                throw new BadRequestError(`Missing ${e} in request body.`)
+            }
+        })
 
+        if(credentials.email.indexOf("@") <= 0) {
+            throw new BadRequestError("Invalid email.")
+        }
         //make sure no user already exists in db with same email
         //if one does throw an error
+        const existingUser = await User.fetchByUserEmail(credentials.email)
+        if(existingUser) {
+            throw new BadRequestError(`Duplicate email: ${credentials.email}`)
+        }
 
         //take users pw and hash it
         //take email and lowercase it
+        const lowercasedEmail = credentials.email.toLowerCase()
 
         //create new user in db with all info
-
+        const result = await db.query(`
+            INSERT INTO users (
+                password,
+                email,
+                first_name,
+                last_name,
+                location
+            )
+            VALUES ($1, $2, $3, $4, $5)
+            RETURNING id, password, email, first_name, last_name, location, date;
+        `, [credentials.password, lowercasedEmail, credentials.first_name, credentials.last_name, credentials.location])
+        
         //return user
+        const user = result.rows[0]
+
+        return user
+    }
+
+    static async fetchByUserEmail(email) {
+        if (!email) {
+            throw new BadRequestError("No email provided")
+        }
+
+        const query = `SELECT * FROM users WHERE email = $1`
+
+        const result = await db.query(query, [email.toLowerCase()])
+
+        const user = result.rows[0]
+
+        return user
     }
 }
 module.exports = User
