9ca08d9dec20f568d3264ca0027950b80fb79ab0
Aligary <aligaryjr@gmail.com>
2022-06-28 11:34:19 -0700

    set up environment variables and config for project

--- vaccine-hub-api/server.js
+++ vaccine-hub-api/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
+const { PORT } = require("./config")
 
 const { BadRequestError, NotFoundError } = require("./utils/errors")
 
@@ -20,8 +21,6 @@ app.use((err, req, res, next) => {
     const status = err.status || 500
     const message = err.message
 })
-
-const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running http://localhost:${PORT}`)
